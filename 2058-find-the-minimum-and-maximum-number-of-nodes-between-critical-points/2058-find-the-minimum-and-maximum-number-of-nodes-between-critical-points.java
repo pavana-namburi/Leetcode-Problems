ca1d164e11d175ecb46544c74b41f67418ae9c0c
Pavana Namburi <pavananamburi1@gmail.com>
2026-08-31 16:15:00 +0530

    Time: 5 ms (40.73%), Space: 106 MB (85.47%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,31 +10,41 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        ArrayList<Integer> al=new ArrayList<>();
         ListNode curr=head;
-        int prev=-1,cnt=0;
+        int prev=-1,cnt=0,st=-1,pc=-1;
+        int res[]=new int[2];
+        res[0]=Integer.MAX_VALUE;
+
         while(curr!=null){
             cnt++;
             if(prev!=-1 && curr.next!=null){
-                if(prev>curr.val && curr.val<curr.next.val)
-                    al.add(cnt);
-                else if(prev<curr.val && curr.val>curr.next.val)
-                    al.add(cnt);
+                if(prev>curr.val && curr.val<curr.next.val){
+                    if(st==-1){
+                        st=cnt;
+                        pc=cnt;
+                    }
+                    else{
+                        res[0]=Math.min(res[0],cnt-pc);
+                        pc=cnt;
+                    }
+                }
+                else if(prev<curr.val && curr.val>curr.next.val){
+                    if(st==-1){
+                        st=cnt;
+                        pc=cnt;
+                    }
+                    else{
+                        res[0]=Math.min(res[0],cnt-pc);
+                        pc=cnt;
+                    }
+                }
             }
             prev=curr.val;
             curr=curr.next;
         }
-        int n=al.size();
-        if(n<2)
+        if(pc==st || st==-1)
             return new int[]{-1,-1};
-
-        int res[]=new int[2];
-        Collections.sort(al);
-        res[0]=al.get(1)-al.get(0);
-        for(int i=2;i<n;i++){
-            res[0]=Math.min(res[0],al.get(i)-al.get(i-1));
-        }
-        res[1]=al.get(n-1)-al.get(0);
+        res[1]=pc-st;
         return res;
     }
 }
