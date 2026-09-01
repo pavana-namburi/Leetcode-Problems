be8c77da5beb55a66072fa74773d768a2d5c76fb
Pavana Namburi <pavananamburi1@gmail.com>
2026-09-01 21:01:41 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.26%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,13 +11,18 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode curr=head;
-        HashSet<ListNode> hm=new HashSet<>();
-        while(curr!=null){
-            if(hm.contains(curr))
-                return curr;
-            hm.add(curr);
-            curr=curr.next;
+        ListNode f=head,s=head;
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+            if(f==s){
+                ListNode p1=s,p2=head;
+                while(p1!=p2){
+                    p1=p1.next;
+                    p2=p2.next;
+                }
+                return p1;
+            }
         }
         return null;
     }
