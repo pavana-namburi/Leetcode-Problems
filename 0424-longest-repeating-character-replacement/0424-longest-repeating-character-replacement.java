3d68df92b595225b51612522dbef0101d7520c15
Pavana Namburi <pavananamburi1@gmail.com>
2026-09-01 16:23:32 +0530

    Time: 32 ms (16.3%), Space: 47.4 MB (7.32%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,10 +1,13 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int n=s.length(),i=0,res=0;
+        int n=s.length(),i=0,res=0,max=0;
         HashMap<Character,Integer> hm=new HashMap<>();
+
         for(int j=0;j<n;j++){
             hm.put(s.charAt(j),hm.getOrDefault(s.charAt(j),0)+1);
-            while(!valid(hm,k)){
+            max=Math.max(max,hm.get(s.charAt(j)));
+
+            while((j-i+1)-max>k){
                 hm.put(s.charAt(i),hm.get(s.charAt(i))-1);
                 if(hm.get(s.charAt(i))==0)
                     hm.remove(s.charAt(i));
@@ -13,13 +16,5 @@ class Solution {
             res=Math.max(res,j-i+1);
         }
         return res;
-    }
-    private boolean valid(HashMap<Character,Integer> hm,int k){
-        int n=hm.size(),max=0,sum=0;
-        for(char x:hm.keySet()){
-            max=Math.max(max,hm.get(x));
-            sum+=hm.get(x);
-        }
-        return (sum-max)<=k;
     }
 }
